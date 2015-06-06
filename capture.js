b3c431e25a41d0506406b2ee1f598d7185679bf8
siso9to <yonepin@gmail.com>
2015-06-07 04:33:10 +0900

    modified site

--- capture.js
+++ capture.js
@@ -1,19 +1,20 @@
 var urls = [
-    "http://www.yahoo.co.jp",
-    "http://google.com",
-    "http://www.facebook.net"
+    "http://www.yahoo.co.jp"
+    ,"http://zozo.jp"
+    ,"http://google.com"
 ];
 
 var casper = require('casper').create();
 casper.start().each(urls, function(self, link) {
     self.thenOpen(link, function() {
         title = this.getTitle();
+        console.log(title);
     })
     .thenEvaluate(function() {
         document.body.bgColor = 'white';
     }, 'setBackGround')
    .then(function() {
-      this.capture(title + '.png');
+        this.capture(title + '.png');
    });
 });
 casper.run();
